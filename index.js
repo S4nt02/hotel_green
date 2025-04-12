0d638f46434db2ca045d53bf6e622e213281200f
Andre Santos <andremsantos711@gmailcom>
2025-04-12 12:00:07 -0300

    rota

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(express.static('/frontend/build'));
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 8080; 
 
