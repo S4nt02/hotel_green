6fe6a531f124a355e12ec0724ab9d0c118462cb0
Andre Santos <andremsantos711@gmailcom>
2025-04-12 01:23:34 -0300

    ./

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-app.use(express.static('frontend/build'));
+app.use(express.static('./frontend/build'));
 app.use(cors())
 const port = process.env.PORT || 8080; 
 
