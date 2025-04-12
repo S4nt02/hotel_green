50c65a1531bb9c2dc63c95d2508644ddb98ebbeb
Andre Santos <andremsantos711@gmailcom>
2025-04-12 11:53:03 -0300

    mudança static

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(express.static('./frontend/build'));
+app.use(express.static('/frontend/build'));
 
 const port = process.env.PORT || 8080; 
 
