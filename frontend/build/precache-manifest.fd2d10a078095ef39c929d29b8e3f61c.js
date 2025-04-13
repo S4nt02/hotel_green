4360b7ae72cf58d80ae1e7767176fd0f9ec1b0a6
Andre Santos <andremsantos711@gmailcom>
2025-04-12 23:41:52 -0300

    build

---
+++ frontend/build/precache-manifest.fd2d10a078095ef39c929d29b8e3f61c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a974ea1fbd6dd7c35300b23f1778bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b6c3a6ab34327b01f91",
+    "url": "/static/css/main.13149329.chunk.css"
+  },
+  {
+    "revision": "307a81a6e17c8fd46da3",
+    "url": "/static/js/2.7dd09c87.chunk.js"
+  },
+  {
+    "revision": "30901cf454a8a9d8029742a84550f279",
+    "url": "/static/js/2.7dd09c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b6c3a6ab34327b01f91",
+    "url": "/static/js/main.568cd326.chunk.js"
+  },
+  {
+    "revision": "0b6b46ef1e17d32e6df4",
+    "url": "/static/js/runtime-main.bce919bb.js"
+  },
+  {
+    "revision": "fa8d32be8dfcdf1e11ea4b213393b026",
+    "url": "/static/media/conteudo_1.fa8d32be.jpg"
+  },
+  {
+    "revision": "b0ce70dedfa020863701be148d67981e",
+    "url": "/static/media/conteudo_2.b0ce70de.jpg"
+  },
+  {
+    "revision": "d17dfd1d77081b44f1f8fb556fb579d8",
+    "url": "/static/media/conteudo_3.d17dfd1d.jpg"
+  },
+  {
+    "revision": "fe031517ce6e5590216a3af72da691a8",
+    "url": "/static/media/entrada_hotel.fe031517.jpg"
+  },
+  {
+    "revision": "bf714b4ae1364dbe922686a1870dde4f",
+    "url": "/static/media/logo_white.bf714b4a.png"
+  }
+]);
